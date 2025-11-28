2fff526199a6f945d9b8c662198890e0d6b052f3
caiomonteiro535@gmail.com <caiomonteiro535@gmail.com>
2025-11-28 00:07:57 -0300

    requirements_atualiza3

--- educlink/urls.py
+++ educlink/urls.py
@@ -10,13 +10,14 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),  # necessário para login social
     path('', include('sistema.urls')),
     path("gerar_plano_ia/", views.gerar_plano_ia, name="gerar_plano_ia"),
-    path('gerar_pdf/', views.gerar_pdf_plano, name='gerar_pdf_plano'),
     path("", views.planoscriados, name="lista"),
 
     # CRIAR PLANO
     path("novo/", views.novo_plano, name="novo"),
 
+    
     # GERAR PDF DO PLANO
+    path('gerar_pdf/', views.gerar_pdf_plano, name='gerar_pdf_plano'),
     path("<int:plano_id>/pdf/", views.gerar_pdf_plano, name="pdf"),
 ]
 
